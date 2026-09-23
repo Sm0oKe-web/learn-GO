54d65745cfe8f20b92913958d2dbeda24a517dfa
tytvtvtao <tytvtvtaoa@gmail.com>
2026-09-23 18:48:38 +0300

    Второй видос

--- main.go
+++ main.go
@@ -1,10 +1,24 @@
-package main
+// package main
+
+// import "fmt"
+
+// func main() {
+
+// 	fmt.Println("Hello World")
+// 	fmt.Println("Привет от GO")
+
+// }
+package main //начанию делать по второму видосу
 
 import "fmt"
 
 func main() {
-
-	fmt.Println("Hello World")
-	fmt.Println("Привет от GO")
-
+	var name string
+	var age int8
+	fmt.Println("What is your name?")
+	fmt.Scan(&name)
+	fmt.Println("Hello " + name + "!")
+	fmt.Println("How old are you?")
+	fmt.Scan(&age)
+	fmt.Println("You are " + fmt.Sprint(age) + "years!")
 }
